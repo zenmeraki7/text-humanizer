d7c64b4fe0597e363786c86a1df08f07a8968ae0
zenmeraki7 <zenmerakiofficial@gmail.com>
2025-09-27 15:59:48 +0530

    Update utils.js

--- frontend/src/components/DetectorComponents/utils.js
+++ frontend/src/components/DetectorComponents/utils.js
@@ -372,7 +372,7 @@ export const getScoreIcon = (score) => {
 };
 
 // ✅ FIXED: Updated backend URL
-const BACKEND_URL = 'http://localhost:8000';
+const BACKEND_URL = 'https://test-3p0b.onrender.com';
 
 // API functions
 export const analyzeText = async (text) => {
